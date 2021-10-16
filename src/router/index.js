0bb27971668271c68988da43fe02493bf63fb296
Harry Kim <akaikarus@real-iv.co.kr>
2021-10-17 03:30:07 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,34 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Main from '@/components/Main'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   mode: 'history',
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Main',
+//       component: Main
+//     }
+//   ]
+// })
+
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Main from '@/components/Main'
 
-Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  }
+]
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Main',
-      component: Main
-    }
-  ]
+const router = createRouter({
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
 })
+
+export default router
